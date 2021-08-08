ce374f584dcaea19f7bddd3c0bead080a0b1cffe
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-08 19:32:59 +0530

    simple test

--- MERN/Full-Stack-App/server/index.js
+++ MERN/Full-Stack-App/server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'})
+    res.send({hi: 'hello'})
 })
 
 const port = process.env.PORT || 5000;
