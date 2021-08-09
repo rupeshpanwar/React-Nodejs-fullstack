9416c50a2044b8ab81be103d7b4a0f176b6aa0dd
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-09 09:37:02 +0530

    Google oAuth

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'its rainy !!!'})
-})
-
+// app.get('/', (req, res) => {
+//     res.send({hi: 'its rainy !!!'})
+// })
+passport.use(new GoogleStrategy({
+    clientID : keys.clientID,
+    clientSecret : keys.clientSecret,
+    callbackURL: '/auth/google/callback'
+},
+accessToken => {
+    console.log(accessToken)
+}));
+// Client ID 85941131422-c7vm8p4f4q94edn7qpahdhutj3m5eaq5.apps.googleusercontent.com
+// secret p9IszkbphBxVNKnabV4ZtJx3
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => `Server running on port ${port} 🔥`);
