3a62f1504d2ddc361a1b3ee91b7d0b95f6f6df46
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-09 13:31:42 +0530

    Server structure refactor

--- server/index.js
+++ server/index.js
@@ -1,36 +1,9 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys')
+require('./services/passport');
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//     res.send({hi: 'its rainy !!!'})
-// })
-passport.use(new GoogleStrategy({
-    clientID : keys.googleClientID,
-    clientSecret : keys.googleSecret,
-    callbackURL: '/auth/google/callback'
-},
-    //console.log(accessToken)
-    (accessToken,refreshToken,profile,done) => {
-        console.log('access token',accessToken);
-        console.log('refresh token',refreshToken);
-        console.log('profile:',profile);
-    }));
+require('./routes/authRoutes')(app);
 
-app.get('/auth/google',
-        passport.authenticate('google',{
-            scope: ['profile','email']
-        })        
-)
-
-app.get('/auth/google/callback',
-        passport.authenticate('google')
-)
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => `Server running on port ${port} 🔥`);
-// https://arcane-retreat-68635.herokuapp.com/ 
-// https://git.heroku.com/arcane-retreat-68635.git
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
