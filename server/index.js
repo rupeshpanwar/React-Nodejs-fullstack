86532014e62dda863f6b8937f45f3f4ac5c0d3bc
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-09 05:50:48 +0530

    basic setup

--- server/index.js
+++ server/index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'how are you !!!'})
+    res.send({hi: 'its rainy !!!'})
 })
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => `Server running on port ${port} 🔥`);
-// https://git.heroku.com/frozen-scrubland-28418.git
-// https://frozen-scrubland-28418.herokuapp.com/ deployed to Heroku
+// https://arcane-retreat-68635.herokuapp.com/ 
+// https://git.heroku.com/arcane-retreat-68635.git
