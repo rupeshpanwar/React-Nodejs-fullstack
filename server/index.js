3b340a02bc1065ed5284fb98e75ff9074ecc9132
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-09 05:47:27 +0530

    sync up

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'how !!!'})
+    res.send({hi: 'how are you !!!'})
 })
 
 const port = process.env.PORT || 5000;
