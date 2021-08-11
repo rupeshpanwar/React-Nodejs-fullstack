1aec19083166408c7bfb8bb45d190056116f9a4e
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-11 12:56:01 +0530

    make it better

---
+++ routes/authRoutes.js
@@ -0,0 +1,24 @@
+const passport = require('passport');
+
+module.exports = app => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
+
+  app.get('/auth/google/callback', passport.authenticate('google'));
+  
+  // logout user - killing the cookie
+  app.get('/api/logout',(req,res) => {
+    req.logout()
+    res.send(req.user)
+  })
+
+  //cookie return route handler -
+  app.get('/api/current_user',(req,res) => {
+    res.send(req.user)
+    // res.send(req.session)
+  })
+};
