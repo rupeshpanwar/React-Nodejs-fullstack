85e7a0224c7da3a1a24f06ec51352cf1a47cc4e2
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-15 18:53:46 +0530

    authReducer return value

--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,6 +1,11 @@
-export default function(state = {}, action) {
+import {FETCH_USER } from '../actions/types'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function(state = null, action) {
   console.log(action);
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false
     default:
       return state;
   }
