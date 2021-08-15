fd5dc079a7e0ecea2ff20c5d1e99ec07c371055c
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-15 12:30:47 +0530

    Reducers

---
+++ server/client/src/reducers/authReducer.js
@@ -0,0 +1,13 @@
+
+export default function(state = {}, action) {
+  switch (action.type) {
+    // case :
+    //   return {
+    //     ...state,
+        
+    //   };
+     
+    default:
+      return state;
+  }
+}
