3022c515dea14fc9b0c5b9b5588ddad67714b1c0
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-18 06:37:37 +0530

    Mailer setup, email template

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -3,5 +3,5 @@ const helper = sendgrid.helper
 const keys = require('../config/keys')
 
 class Mailer extends helper.Mail{
-    
+    // this.from_email = new helper.Email('no-reply@emaily.com');
 }
