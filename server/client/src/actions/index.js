7fe5a5d761697f93af96cebf941a9213d8d8dc93
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-09-03 07:06:07 +0530

    submit survey

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,14 +1,18 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => async dispatch => {
+export const fetchUser = () => async (dispatch) => {
   const res = await axios.get('/api/current_user');
 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const handleToken = token => async dispatch => {
+export const handleToken = (token) => async (dispatch) => {
   const res = await axios.post('/api/stripe', token);
 
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const submitSurvey = (values) => async (dispatch) => {
+  return { type: 'submit_survey' };
 };
